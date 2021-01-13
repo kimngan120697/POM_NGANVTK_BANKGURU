ea5dc72db04d54972f899cd807a68ee2a9b95497
NganVTK <kimngan19973312@gmail.com>
2021-01-13 09:25:00 +0700

    k

--- testcases/com/bankguru/payment/Payment_001.java
+++ testcases/com/bankguru/payment/Payment_001.java
@@ -13,11 +13,10 @@ public class Payment_001 {
 
 	@BeforeTest
 	public void beforeTest() {
-		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
-		driver = new ChromeDriver();
-//		System.setProperty("webdriver.gecko.driver", ".\\libraries\\geckodriver.exe");
-//
-//		driver=new FirefoxDriver();
+//		System.setProperty("webdriver.chrome.driver", ".\\browserDriver\\chromedriver.exe");
+//		driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", ".\\browserDriver\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		driver.get("https://github.com/kimngan120697/POM_NGANVTK_BANKGURU");
 	}
 
@@ -25,43 +24,52 @@ public class Payment_001 {
 	public void Payment_01_CreateNewCustomer() {
 
 	}
+
 	@Test
 	public void Payment_02_EditCustomer() {
 
 	}
+
 	@Test
 	public void Payment_03_AddNewAccount() {
 
 	}
+
 	@Test
 	public void Payment_04_EditAccount() {
 
 	}
+
 	@Test
 	public void Payment_05_DepositToCurrentAccount() {
 
 	}
+
 	@Test
 	public void Payment_06_WithDrawFromCurrentAccount() {
 
 	}
+
 	@Test
 	public void Payment_07_TransferToAnotherAccount() {
 
 	}
+
 	@Test
 	public void Payment_08_CheckAccountBalance() {
 
 	}
+
 	@Test
 	public void Payment_09_DeleteAllAccount() {
 
 	}
+
 	@Test
 	public void Payment_10_DeleteCustomer() {
 
 	}
-	
+
 	@AfterTest
 	public void afterTest() {
 	}
